17fba4364714bbcf80f6aa300ffce69814042bc0
sc0ncept <sebconcept@outlook.com>
2020-10-27 19:58:07 +0100

    Add cours way to prevent duplicate quotes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const quote = document.querySelector('#quote');
 const btn = document.querySelector('#get-quote');
 const quotes = [];
 
-// My solution
 const showQuote = function () {
 
   fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
@@ -14,18 +13,32 @@ const showQuote = function () {
       }
     }).then(function (data) {
 
-      if (quotes.length >= 50) {
-        quotes.length = 0;
+      // Course way
+      if (quotes.indexOf(data[0]) > -1) {
         showQuote();
-      } else {
-
-        if (quotes.indexOf(data[0]) === -1) {
-          quotes.push(data[0]);
-          quote.textContent = quotes[quotes.length - 1];
-        } else {
-          showQuote();
-        }
+        return;
       }
+
+      quote.textContent = data[0];
+      quotes.push(data[0]);
+
+      if (quotes.length >= 50) {
+        quotes.length = 0; // quotes = []; did not work properly
+      }
+
+      // My way
+      // if (quotes.length >= 50) {
+      //   quotes.length = 0;
+      //   showQuote();
+      // } else {
+
+      //   if (quotes.indexOf(data[0]) === -1) {
+      //     quotes.push(data[0]);
+      //     quote.textContent = quotes[quotes.length - 1];
+      //   } else {
+      //     showQuote();
+      //   }
+      // }
 
     }).catch(function (err) {
       quote.textContent = '[Something went wrong, sorry!] I have a joke for you... The government in this town is excellent, and uses your tax dollars efficiently.';
